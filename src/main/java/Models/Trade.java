64aff4e34d233b9e5ffc4bb45fa41c52fd7f58c3
D1R2 <d1r2.main@gmail.com>
2018-08-09 10:57:38 -0400

    Updated trade class with added attributes.

--- src/main/java/Models/Trade.java
+++ src/main/java/Models/Trade.java
@@ -16,74 +16,159 @@ public class Trade {
 
     // INSTANCE VARIABLES
 
-    private int number;
-    private String type;
-    private LocalDateTime entryTime;
-    private LocalDateTime exitTime;
+    private int tradeNumber;
+    private String instrument;
+    private String account;
+    private String strategy;
+    private String marketPosition;
+    private int qty;
     private double entryPrice;
     private double exitPrice;
+    private LocalDateTime entryDate;
+    private LocalDateTime entryTime;
+    private LocalDateTime exitDate;
+    private LocalDateTime exitTime;
 
 
     // CONSTRUCTOR
 
-    public Trade(int number, String type, LocalDateTime entryTime, LocalDateTime exitTime, double entryPrice, double exitPrice) {
-        this.number = number;
-        this.type = type;
-        this.entryTime = entryTime;
-        this.exitTime = exitTime;
+    public Trade(int tradeNumber, String instrument, String account, String strategy, String marketPosition, int qty,
+                 double entryPrice, double exitPrice, LocalDateTime entryDate, LocalDateTime entryTime, LocalDateTime exitDate,
+                 LocalDateTime exitTime) {
+        this.tradeNumber = tradeNumber;
+        this.instrument = instrument;
+        this.account = account;
+        this.strategy = strategy;
+        this.marketPosition = marketPosition;
+        this.qty = qty;
         this.entryPrice = entryPrice;
         this.exitPrice = exitPrice;
+        this.entryDate = entryDate;
+        this.entryTime = entryTime;
+        this.exitDate = exitDate;
+        this.exitTime = exitTime;
     }
 
 
     // GETTERS AND SETTERS
 
-    public int getNumber() {
-        return number;
+    public int getTradeNumber() {
+        return tradeNumber;
     }
 
-    public void setNumber(int number) {
-        this.number = number;
+
+    public void setTradeNumber(int tradeNumber) {
+        this.tradeNumber = tradeNumber;
     }
 
-    public String getType() {
-        return type;
+
+    public String getInstrument() {
+        return instrument;
     }
 
-    public void setType(String type) {
-        this.type = type;
+
+    public void setInstrument(String instrument) {
+        this.instrument = instrument;
     }
 
-    public LocalDateTime getEntryTime() {
-        return entryTime;
+
+    public String getAccount() {
+        return account;
     }
 
-    public void setEntryTime(LocalDateTime entryTime) {
-        this.entryTime = entryTime;
+
+    public void setAccount(String account) {
+        this.account = account;
     }
 
-    public LocalDateTime getExitTime() {
-        return exitTime;
+
+    public String getStrategy() {
+        return strategy;
     }
 
-    public void setExitTime(LocalDateTime exitTime) {
-        this.exitTime = exitTime;
+
+    public void setStrategy(String strategy) {
+        this.strategy = strategy;
     }
+
+
+    public String getMarketPosition() {
+        return marketPosition;
+    }
+
+
+    public void setMarketPosition(String marketPosition) {
+        this.marketPosition = marketPosition;
+    }
+
+
+    public int getQty() {
+        return qty;
+    }
+
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
 
     public double getEntryPrice() {
         return entryPrice;
     }
 
+
     public void setEntryPrice(double entryPrice) {
         this.entryPrice = entryPrice;
     }
+
 
     public double getExitPrice() {
         return exitPrice;
     }
 
+
     public void setExitPrice(double exitPrice) {
         this.exitPrice = exitPrice;
+    }
+
+
+    public LocalDateTime getEntryDate() {
+        return entryDate;
+    }
+
+
+    public void setEntryDate(LocalDateTime entryDate) {
+        this.entryDate = entryDate;
+    }
+
+
+    public LocalDateTime getEntryTime() {
+        return entryTime;
+    }
+
+
+    public void setEntryTime(LocalDateTime entryTime) {
+        this.entryTime = entryTime;
+    }
+
+
+    public LocalDateTime getExitDate() {
+        return exitDate;
+    }
+
+
+    public void setExitDate(LocalDateTime exitDate) {
+        this.exitDate = exitDate;
+    }
+
+
+    public LocalDateTime getExitTime() {
+        return exitTime;
+    }
+
+
+    public void setExitTime(LocalDateTime exitTime) {
+        this.exitTime = exitTime;
     }
 
 }
